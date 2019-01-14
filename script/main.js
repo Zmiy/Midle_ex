fd7881807b046fad5ce3db7373ecc30c59fcc65c
Evgeny Drapkin <drapkine@gmail.com>
2019-01-14 14:14:38 +0200

    js and litle changes in html and css

---
+++ script/main.js
@@ -0,0 +1,18 @@
+var lastIndex = 0;
+
+window.onload = init;
+
+function init() {
+	var interval = setInterval(handleRefresh, 3000);
+	handleRefresh();
+}
+
+function handleRefresh()
+{
+    var images = ["250X110668568400.jpg", "banner_atar.jpg","paz_250x110059890678.jpg", "paz_250x110103131473.jpg", "paz_250X110571325734.jpg"];
+    document.getElementById("changedImage").src = "../img/" +images[lastIndex++];
+    if (lastIndex==images.length)
+    {
+        lastIndex=0;
+    }
+}
