6b13e5dd94b3bb90aa3e3167fcda7df1b3981ff4
Evgeny Drapkin <drapkine@gmail.com>
2019-01-14 17:08:13 +0200

    js next

--- script/main.js
+++ script/main.js
@@ -10,7 +10,7 @@ function init() {
 function handleRefresh()
 {
     var images = ["250X110668568400.jpg", "banner_atar.jpg","paz_250x110059890678.jpg", "paz_250x110103131473.jpg", "paz_250X110571325734.jpg"];
-    document.getElementById("changedImage").src = "Midle_ex/img/" +images[lastIndex++];
+    document.getElementById("changedImage").src = "img/" +images[lastIndex++];
     console.log(document.getElementById("changedImage").src);
     if (lastIndex==images.length)
     {
